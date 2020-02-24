014033d5833a7391e11c5bdabd916a570a4d3c14
Deepika Sharma <Deepikaasharma@outlook.com>
2020-02-24 13:25:38 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Years of experience do you have?");
+
+            var Years = 0;
+
+            if (Years <= 0)
+            {
+                Console.WriteLine("You are a Inexperienced");
+            }
+            else if (Years == 1)
+            {
+                Console.WriteLine("You are a Junior");
+            }
+            else if (Years == 2)
+            {
+                Console.WriteLine("You are an Intermediate");
+            }
+            else if (Years == 3) 
+            {
+                Console.WriteLine("You are Advanced");
+            }
+            else if (Years >= 4)
+            {
+                Console.WriteLine("You are Senior");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
